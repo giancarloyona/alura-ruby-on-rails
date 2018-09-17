30bbb80f9fd3c937a70f8b1d636f5520d6150952
Giancarlo Angelis <gianyona@gmail.com>
2018-09-17 13:23:12 -0300

    finishing unit 8

--- loja/config/routes.rb
+++ loja/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  #get "produtos" => "produtos#index"
+  
+  get "/produtos/new" => "produtos#new"
+  post "/produtos" => "produtos#create"
+  
   root "produtos#index"
+
 end
