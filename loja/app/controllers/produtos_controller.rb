8155ca8ea61fef404da11f86c12a86e5014a0f5e
giancarloyona <gianyona@gmail.com>
2018-09-19 23:01:54 -0300

    refactoring the code

--- loja/app/controllers/produtos_controller.rb
+++ loja/app/controllers/produtos_controller.rb
@@ -1,35 +1,66 @@
 class ProdutosController < ApplicationController
 
-    def new
-        @produto = Produto.new
-        @departamentos = Departamento.all
-    end
+    before_action :set_produto, only: [:update, :destroy, :edit]
 
     def index 
         @produtos_por_nome = Produto.order(:nome)
         @produtos_por_preco = Produto.order(:preco).limit(2)
     end
 
+
+    def new
+        @produto = Produto.new
+        @departamentos = Departamento.all
+    end
+
     def create
-        valores = params.require(:produto).permit(:nome, :preco, :quantidade, :descricao, :departamento_id)
-        @produto = Produto.new valores
+        @produto = Produto.new produto_params
         if @produto.save
             flash[:notice] = "Produto salvo com sucesso!"
             redirect_to(root_url)
         else
-            render :new
+            renderiza :new
         end
     end
 
-    def destroy
-        id = params[:id]
-        Produto.destroy(id)
-        redirect_to(root_url)
-    end 
+    def edit
+        set_produto
+        renderiza :edit
+    end
+
+    def update
+        if @produto.update produto_params
+            flash[:notice] = "Produto atualizado com sucesso"
+            redirect_to root_url
+        else
+            renderiza :new
+        end
+    end
 
     def busca
         @nome = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome}%"
+    end
+
+    def destroy
+        @produto.destroy
+        redirect_to(root_url)
+    end 
+
+    private 
+
+    def renderiza(view)
+        @departamentos = Departamento.all
+        render view
+    end
+
+    def set_produto 
+        id = params[:id]
+        @produto = Produto.find(id)
+    end
+
+    def produto_params
+        params.require(:produto).permit(:nome, :preco, :quantidade, :descricao, :departamento_id)
     end
 
 end
