ba2ea78d0b799fe8d00413171c96d99b2c73972b
giancarloyona <gianyona@gmail.com>
2018-09-19 21:37:39 -0300

    adding a department

--- loja/config/routes.rb
+++ loja/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
+  resources :departamentos
   resources :produtos, only:[:new,:create,:destroy]
+  
   get "/produtos/busca" => "produtos#busca", as: :busca_produto
   root "produtos#index"
 
